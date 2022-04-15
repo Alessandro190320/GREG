3be7c282d1036425ec25eaad9c36d8afbb39a7a6
Tepag <z190tpg@gmail.com>
2022-04-15 23:08:00 +0200

    roviniamo la fork

--- GREG-Unity/Assets/Scripts/SceneTransition.cs
+++ GREG-Unity/Assets/Scripts/SceneTransition.cs
@@ -11,13 +11,28 @@ public class SceneTransition : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             int indexFloor = SceneManager.GetActiveScene().buildIndex;
-            if (name.Contains("up"))
-                indexFloor++;
-            else if (name.Contains("down"))
-                indexFloor--;
+            int indexFloorLiceo = SceneManager.GetActiveScene().buildIndex;
+
+            // se nell'opggetto contiene licero, vado a selezionare iil piano per quanto riguarda il liceo
+            // in caso contrario spostarsi sul piano dell'itis
+            if(!name.Contains("liceo")){
+                if (name.Contains("up"))
+                    indexFloor++;
+                else if (name.Contains("down"))
+                    indexFloor--;
+            }
+            else {
+                if (name.Contains("up"))
+                    indexFloorLiceo++;
+                else if (name.Contains("down"))
+                    indexFloorLiceo--;
+            }
 
             PlayerState.setPosition(initialPosition);
             LevelSystem.current.ChangeScene(indexFloor);
         }
     }
 }
+// Sessione remota test 15-04-2022 
+// ciao David 
+// ciao Marcheselli
